bfd25eb0eac11eb9b299e90fc6eb18039b941524
gabrieltino <miracle8093@gmail.com>
2019-04-22 12:12:53 +0100

    worked on responsiveness

---
+++ index.php
@@ -0,0 +1,280 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Hallel College Clone</title>
+  <link rel="stylesheet" href="./css/bootstrap.min.css">
+  <link rel="stylesheet" href="./css/main.css">
+</head>
+
+<body>
+  <!-- <nav class="navbar navbar-expand-lg navbar-light stickynav">
+    <a class="navbar-brand" href="#">
+      <img src="./images/logo.png" class="responsive-logo" alt="Logo">
+    </a>
+    <button class="navbar-toggler mr-2" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse offset-lg-3" id="navbarText">
+      <ul class="navbar-nav setnavbar-sm">
+        <li class="nav-item pr-3 pl-3 active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item pl-3">
+          <a class="nav-link" href="#">Discover Hallel</a>
+        </li>
+        <li class="nav-item pl-3">
+          <a class="nav-link" href="#">Academics</a>
+        </li>
+        <li class="nav-item pl-3">
+          <a class="nav-link" href="#">Admission</a>
+        </li>
+        <li class="nav-item pl-3">
+          <a class="nav-link" href="#">Gallery</a>
+        </li>
+        <li class="nav-item pr-1 pl-3">
+          <a class="nav-link" href="#">Contacts</a>
+        </li>
+      </ul>
+    </div>
+  </nav> -->
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+  <a class="navbar-brand" href="#">
+      <img src="./images/logo.png" class="responsive-logo" alt="Logo">
+    </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+  <ul class="navbar-nav setnavbar-sm offset-lg-2">
+        <li class="nav-item pr-3 active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item pl-3">
+          <a class="nav-link" href="#">Discover Hallel</a>
+        </li>
+        <li class="nav-item pl-3">
+          <a class="nav-link" href="#">Academics</a>
+        </li>
+        <li class="nav-item pl-3">
+          <a class="nav-link" href="#">Admission</a>
+        </li>
+        <li class="nav-item pl-3">
+          <a class="nav-link" href="#">Gallery</a>
+        </li>
+        <li class="nav-item pr-1 pl-3">
+          <a class="nav-link" href="#">Contacts</a>
+        </li>
+      </ul>
+  </div>
+</nav>
+
+  <div class="container-fluid">
+    <div class="row">
+      <div id="carouselExampleIndicators" class="carousel slide checkmg" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="./images/slideimg1.jpg" class="d-block w-100 sliderimage" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="./images/slideimg2.jpg" class="d-block w-100 sliderimage" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="./images/slideimg3.jpg" class="d-block w-100 sliderimage" alt="...">
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+      <div class="col-6 bg-danger text-white p-2">
+        <span class="pr-lg-3 pr-1">+2348033409685,</span>
+        <span class="pr-lg-3 pr-1">+2348033409685,</span>
+      </div>
+      <div class="col-6 hallelgray p-2">
+        <marquee>rweeeeeeeeeeeeeeeeeeeeee</marquee>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12 welcomebar text-center mx-auto mt-5 mb-4 text-white">Welcome To Hallel College</div>
+      <section class="px-5 col-12">
+        <p>
+          HALLEL COLLEGE is a world-class private Nursery, Primary, Secondary (Day and Boarding) and Sixth Form College in the centre of the bustling city of Port Harcourt.
+        </p>
+        <p>
+          For over 20 years, we have been blessed with outstanding results in our mission to empower the total youth for tomorrow's challenges through quality education.
+        </p>
+        <p>
+          As a proud British Council International School Award winner, we are passionate about excellence in academics, socio-political development, global citizenship, character moulding and godly living.
+        </p>
+        <p>
+          Our array of enrichment programs, equip our students with the skills to broaden the scope of their academic experience while encouraging brilliance outside the classroom.
+        </p>
+        <p>
+          We invite you to visit any of our campuses, where you will find a friendly, peaceful and fostering environment for personal development.
+        </p>
+      </section>
+    </div>
+    <Section class="row mx-auto">
+      <div class="col-12 col-sm-12 col-md-6 col-lg-4">
+        <img src="./images/creche.jpg" class="imageborder" alt="creche class">
+        <div class="px-4 py-2 py-lg-3">
+          <h6 class="text-primary"><a href="#" class="text-decoration-none">CRECHE, NURSERY & PRIMARY SCHOOL</a></h6>
+        </div>
+      </div>
+      <div class="col-12 col-sm-12 col-md-6 col-lg-4">
+        <img src="./images/creche.jpg" class="imageborder" alt="creche class">
+        <div class="px-4 py-2 py-lg-3">
+          <h6 class="text-primary"><a href="#" class="text-decoration-none">JUNIOR AND SENIOR SECONDARY</a></h6>
+        </div>
+      </div>
+      <div class="col-12 col-sm-12 col-md-6 col-lg-4">
+        <img src="./images/creche.jpg" class="imageborder" alt="creche class">
+        <div class="px-4 py-2 py-lg-3">
+          <h6 class="text-primary"><a href="#" class="text-decoration-none">HALLEL SIXTH FORM</a></h6>
+        </div>
+    </section>
+    <section class="row bg-lavender py-4 py-lg-5">
+      <div class="col-12 col-sm-12 col-md-6 col-lg-6 aboutmis">
+        <!-- <img src="./images/aboutmis.jpg" class="d-block"> -->
+      </div>
+      <div class="col-md-6 col-lg-6 text-center text-gray">
+        <h3><b>MISSION & VISION</b></h3>
+        <h6 class="pt-2"><strong>OUR VISION</strong></h6>
+        <p>Raising the total youth through comprehensive education.</p>
+        <h6><strong>OUR MISSION STATEMENT</strong></h6>
+        <p>Mobilizing Academic, Moral, Social, Political and Religious tools, by dedicated and vision driven educators in a proactive environment of teaching and learning, which is geared towards impacting and equipping our students to be THE TOTAL YOUTH the world will be proud of.</p>
+        <h6 class="pt-2"><strong>OUR CORE VALUES</strong></h6>
+        <span>Love, Faith, Hard work, Integrity, Respect, Discipline and Service.</span>
+      </div>
+    </section>
+    <section class="row mt-3">
+      <div class="col-12 p-5 sectionfour">
+        <h2 class="text-center">There is more to life at Hallel College than just bagging<br> certificates, awards and diplomas – We also want our<br> students to excel within and outside the classroom.</h2>
+        <a href="#" class="btn btn-myprimary p-2 mt-4 middle">Education Enrichment Programs</a>
+      </div>
+    </section>
+    <sectiion class="row pt-2">
+      <div class="col-5 col-lg-3 my-auto affliation">
+        <h2>Our Affliations</h2>
+      </div>
+      <div class="col-1 col-lg-2">
+        <img src="./images/partner1.jpg" class=" ml-1 ml-lg-2 partner1" alt="partner1">
+      </div>
+      <div class="col-1 col-lg-2">
+        <img src="./images/partner2.jpg" class="ml-1 ml-lg-2 partner2" alt="partner2">
+      </div>
+    </sectiion>
+    <section class="row">
+      <div class="col-12 col-lg-6 learn-trial">
+        <!-- <img src="./images/learn-trial.jpg"> -->
+      </div>
+      <div class="col-12 col-lg-6 sectionfour text-center">
+        <h2 class="text-center mt-2 mt-lg-3">BECOME A "HALLEL"</h2>
+        <a href="#" class="btn btn-myprimary p-2 mt-2">Apply Now</a>
+        <a href="#" class="btn bg-orange text-white p-2 mt-2 ml-4">Student Life at Hallel</a>
+
+        <h4 class="mt-3">ASSURED QUALITY EDUCATION.</h4>
+        <p class="text-gray text-justify">Make the right choice to receive outstanding value and education as you begin your Hallel experience</p>
+
+        <h4 class="mt-2">AWARD WINNING STUDENTS, TEACHERS AND SCHOOL.</h4>
+        <p class="text-gray text-justify">We are dedicated to ensuring that all our students can favorably compete at their levels of education.</p>
+
+        <h4>BUILD A STRONG ACADEMIC FOUNDATION</h4>
+        <p class="text-gray text-justify">Our curriculum and activities are structured to build a solid educational foundation for our children while challenging them to think critically out of the box.</p>
+
+        <a href="#" class="btn btn-myprimary p-2 mt-3 float-left">Hallel Portal - Sign in</a>
+      </div>
+    </section>
+
+    <section class="row">
+      <div class="shoelaceimage"></div>
+      <div class="col-12 bottom-nav">
+        <div class="row">
+          <div class="col-12 mt-3 mt-lg-0 col-lg-3">
+            <h5 class="text-white">HALLEL COLLEGE</h5>
+            <p class="text-gray">No other time is the establishment of a school with qualitative education more necessary that now.<br><b>HALLEL COLLEGE</b> meets the need of the 21st century pupil and student. We believe every child can excel.
+              Trust us, God has done that through us .</p>
+          </div>
+          <div class="col-12 mt-3 mt-lg-0 col-md-4 col-lg-3">
+            <h5 class="text-white">DISCOVER HALLEL</h5>
+            <ul class="discover">
+              <li><a class="text-gray" href="#">Quick Facts</a></li>
+              <li><a class="text-gray" href="#">About Us</a></li>
+              <li><a class="text-gray" href="#">Downloadable & Forms</a></li>
+            </ul>
+          </div>
+          <div class="col-12 mt-3 mt-lg-0 col-md-4 col-lg-3">
+            <h5 class="text-white">OUR SOCIALS</h5>
+            <p class="text-gray">Follow us on our social media handles</p>
+          </div>
+          <div class="col-12 mt-3 mt-lg-0 col-md-4 col-lg-3">
+            <h5 class="text-white">CONTACT US</h5>
+            <ul class="discover2 setwidth">
+              <li class="text-gray"><span>8 Mini-Ezekwu Street, Rumuogba, Port Harcourt, Rivers State, Nigeria</span>
+              </li>
+              <li class="text-gray mt-3"><span>Km 16 Airport Road, Port Harcourt, Rivers State, Nigeria
+              </li>
+
+              <li class="text-gray mt-3">
+                <span>+234-(0)-8037106151</span><br>
+                <span class="ml-4 pl-3">+234-(0)-8037106151</span><br>
+                <span class="ml-4 pl-3">+234-(0)-8037106151</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+    <footer class="row py-4">
+      <div class="col-12 col-md-9 col-lg-10 sm-text-center">Copyright © HALLEL COLLEGE.</div>
+      <div class="col-12 col-md-3 col-lg-2 sm-text-center">Powered by Me</div>
+    </footer>
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  </div>
+  <script src="./js/jquery.min.js"></script>
+  <script>
+    $(window).on('scroll', function() {
+      if ($(window).scrollTop()) {
+        $('nav').addClass('nav-dark')
+      } else {
+        $('nav').removeClass('nav-dark')
+      }
+    });
+  </script>
+  <script src="./js/bootstrap.min.js"></script>
+  <script src="./js/main.js"></script>
+</body>
+
+</html>
